9793ac7f9af2c3bd9001a19cd9290825b7c2c306
UNCROOS <any492266@gmail.com>
2024-05-10 00:07:39 +0900

    init

--- mama_bob_jua/lib/main_screen.dart
+++ mama_bob_jua/lib/main_screen.dart
@@ -13,15 +13,72 @@ class _main_screenState extends State<main_screen> {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "밥 쳐 먹어",
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'ShillaCulture',
+            Container(
+              width: 393,
+              height: 160,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(color: Color(0xFFFF3838)),
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment(0, 0.5),
+                    child: Text(
+                      "밥줘",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 60,
+                        fontFamily: 'ShillaCulture',
+                      ),
+                    ),
+                  ),
+                ], //children
               ),
             ),
+            SizedBox(height: 30),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: 393,
+                height: 80,
+                clipBehavior: Clip.antiAlias,
+                decoration: ShapeDecoration(
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(48),
+                      bottomRight: Radius.circular(48),
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 185,
+                      top: 28,
+                      child: Container(
+                        width: 24,
+                        height: 24,
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(),
+                        child: FlutterLogo(),
+                      ),
+                    ),
+                    Positioned(
+                      left: 309,
+                      top: 30,
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        child: Stack(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+
           ],
         ),
       ),
